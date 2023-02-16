384074284d96f8420f52a16b6fcee478b495dd36
rla <rla2838@naver.com>
2023-02-16 09:32:20 +0900

    고객파트: 로그인 페이지, 회원가입 페이지 구현, 로그아웃

--- src/main/java/com/example/security/SecurityConfig.java
+++ src/main/java/com/example/security/SecurityConfig.java
@@ -1,15 +1,141 @@
 package com.example.security;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import com.example.security.service.CustomUserDetailsService;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private CustomUserDetailsService userDetailsService;
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
-			.authorizeRequests().antMatchers("*").permitAll();
+		.csrf().disable()
+		.authorizeRequests()
+		.antMatchers("/").permitAll()
+		.antMatchers("/cus").permitAll()
+		.antMatchers("/cus/register", "/cus/checkId","/cus/search-id","/cus/search-pw","/cus/registered").permitAll()
+		.antMatchers("/emp/register", "/emp/registered").permitAll()
+		.antMatchers("/cus/login", "/emp/login", "/admin/login").permitAll()
+		.antMatchers("/logout").authenticated()
+		.antMatchers("/cus/**").hasAnyRole("USER", "ADMIN")
+		.antMatchers("/emp/**").hasAnyRole("EMPLOYEE", "ADMIN")
+		.antMatchers("/admin/**").hasRole("ADMIN")
+		.anyRequest().authenticated()
+		.and()
+		.formLogin()
+		.loginProcessingUrl("/login")
+		.and()
+		.logout()
+		.logoutUrl("/logout")
+		.logoutSuccessUrl("/cus")
+		.and()
+		.addFilter(authenticationFilter())
+		.exceptionHandling()
+		.accessDeniedHandler(accessDeniedHandler())
+		.and()
+		.headers().frameOptions().disable();
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.authenticationProvider(authenticationProvider());
+	}
+	
+	@Bean
+	public AccessDeniedHandler accessDeniedHandler() {
+		return new AccessDeniedHandler() {
+			
+			@Override
+			public void handle(HttpServletRequest request, HttpServletResponse response,
+					AccessDeniedException accessDeniedException) throws IOException, ServletException {
+				response.sendRedirect("/access-denied");
+			}
+		};
+	}
+	
+	@Bean
+	public CustomAuthenticationFilter authenticationFilter() throws Exception {
+		CustomAuthenticationFilter filter = new CustomAuthenticationFilter();
+		filter.setAuthenticationManager(authenticationManager());
+		filter.setAuthenticationSuccessHandler(authenticationSuccessHandler());
+		filter.setAuthenticationFailureHandler(authenticationFailureHandler());
+		return filter;
+	}
+	
+	@Bean
+	public AuthenticationProvider authenticationProvider() {
+		Map<String, UserDetailsService> map = new HashMap<>();
+		map.put("사용자", userDetailsService);
+		
+		return new CustomAuthenticationProvider(map, passwordEncoder);
+	}
+	
+	@Bean
+	public AuthenticationSuccessHandler authenticationSuccessHandler() {
+		return new AuthenticationSuccessHandler() {
+
+			@Override
+			public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+					Authentication authentication) throws IOException, ServletException {
+				CustomAuthenticationToken customAuthenticationToken = (CustomAuthenticationToken) authentication;
+				
+				String userType = customAuthenticationToken.getUserType();
+				if ("사용자".equals(userType)) {
+					response.sendRedirect("/cus");
+				} else if ("직원".equals(userType)) {
+					response.sendRedirect("/emp/home");
+				} else if ("관리자".equals(userType)) {
+					response.sendRedirect("/admin/home");
+				}
+			}			
+		};
+	}
+	
+	@Bean
+	public AuthenticationFailureHandler authenticationFailureHandler() {
+		return new AuthenticationFailureHandler() {
+			
+			@Override
+			public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+					AuthenticationException exception) throws IOException, ServletException {
+				
+				String userType = request.getParameter("userType");
+				if ("사용자".equals(userType)) {
+					response.sendRedirect("/cus/login?error=fail");
+				} else if ("직원".equals(userType)) {
+					response.sendRedirect("/emp/login?error=fail");
+				} else if ("관리자".equals(userType)) {
+					response.sendRedirect("/admin/login?error=fail");
+				}
+			}
+		};
 	}
 }
--- src/main/webapp/WEB-INF/views/cus/success.jsp
+++ src/main/webapp/WEB-INF/views/cus/success.jsp
@@ -6,13 +6,20 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
-<title></title>
+<title>애플리케이션</title>
 </head>
 <body>
-<c:set var="menu" value="home" />
+<c:set var="menu" value="register" />
 <%@ include file="common/navbar.jsp" %>
 <div class="container">
-<h1>하이</h1>
+	<div class="row mb-3">
+		<div class="col-12">
+			<div class="border p-3 bg-light">
+				<h1 class="mb-4">회원가입 완료</h1>
+				<p>회원가입이 완료되었습니다.</p>
+			</div>
+		</div>
+	</div>
 </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
